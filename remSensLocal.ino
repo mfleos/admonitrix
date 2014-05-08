9fd68791b9a286ebc54ad67208d3bfa9fcaab802
Eden Candelas <e.candelas@gmail.com>
2014-05-08 10:05:30 -0500

    [remSensLocal]
    ligth version of remSens with out the network code.
    
    makeString() - modified to create a json format string with the readings values, ID and the time at the string creation moment. Works ok.
    
    URLEncode() - encodes the json, taked from http://hardwarefun.com/tutorials/url-encoding-in-arduino. When passing the result to serial.print() is being truncated at character 315.

--- remSensLocal.ino
+++ remSensLocal.ino
@@ -11,16 +11,14 @@
 #define SEND_TIME 60
 #define MULTIPLIER 100
 
-
-
-
 //Identificador equipo
-
 String ID = "1";
 int lstTemp[] = {0,0,0,0,0,0,0,0,0,0,0,0};
 unsigned long lstTime[] = {0,0,0,0,0,0,0,0,0,0,0,0};
 unsigned long initMillisRead = 0;
 unsigned long initMillisSend = 0;
+int numReads = 0;
+
 
 void setup() {
   // start the Ethernet,UDP and Serial:
@@ -33,20 +31,28 @@ void setup() {
   Serial.println(initMillisRead);
   Serial.print("initMillisSend: ");
   Serial.println(initMillisSend);
+
+
+
 }
 
 void loop() {
   if (timer(READ_TIME, initMillisRead) == 1){
     takeRead(SENS);
+    numReads += 1;
     initMillisRead = millis();
   }
-  if (timer(SEND_TIME, initMillisSend) == 1){
-    //udpSendme(makeString());
-    printValues();
-    String info = makeString();
-    Serial.println(info);
-    initMillisSend = millis();
-    Serial.println(initMillisSend);
+  if (numReads == 12){
+      printValues();
+      String json = makeString();
+      Serial.println(json);
+      Serial.println("-----------------------");
+      unsigned int tamano = 300;
+      char jsonChar[tamano];
+      json.toCharArray(jsonChar, tamano);    
+      Serial.println(URLEncode(jsonChar));      
+      Serial.println(initMillisSend);
+      numReads = 0;
   }
 }
 
@@ -87,12 +93,12 @@ void takeRead(int sensor){
   Serial.print(lstTemp[i]);
   Serial.print("-");
   Serial.println(lstTime[x]); 
-  if(i < (sizeof(lstTemp)/sizeof(int))+ 1){
+  if(i < (sizeof(lstTemp)/sizeof(int))- 1){
     i += 1;
   }else{
     i = 0;  
   }
-  if( x < (sizeof(lstTime)/sizeof(unsigned long)) +1){
+  if( x < (sizeof(lstTime)/sizeof(unsigned long)) -1){
     x += 1;
   }else{
     x = 0;
@@ -109,3 +115,27 @@ void printValues(){
   }
 }
 
+char *convertToChar(String from, char* buffer, unsigned int sizeBuffer){
+  from.toCharArray(buffer, sizeBuffer);
+  return buffer;t
+}
+
+String URLEncode(const char* msg)
+{
+    const char *hex = "0123456789abcdef";
+    String encodedMsg = "";
+
+    while (*msg!='\0'){
+        if( ('a' <= *msg && *msg <= 'z')
+                || ('A' <= *msg && *msg <= 'Z')
+                || ('0' <= *msg && *msg <= '9') ) {
+            encodedMsg += *msg;
+        } else {
+            encodedMsg += '%';
+            encodedMsg += hex[*msg >> 4];
+            encodedMsg += hex[*msg & 15];
+        }
+        msg++;
+    }
+    return encodedMsg;
+}
